621cddc5850a7d3e0f305f4419b3b322ea1278f6
joshh450 <joshh450@yahoo.com>
2018-10-18 10:35:00 -0600

    pre heroku commit

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_heroku import Heroku
 
 app = Flask(__name__)
+app.config['SQLACHEMY_DATABASE_URI'] = ''
+heroku = Heroku(app)
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, email):
+        self.email = email
+
+    def __repr__(self):
+        return '<E-mail %r>' % self.email
 
 @app.route('/')
 def index():
@@ -9,6 +26,27 @@ def index():
 @app.route('/show')
 def show():
     return render_template('show.html')
+
+@app.route('/collect')
+def collect():
+    return render_template('collect.html')
+
+@app.route('/prereg', methods=['POST'])
+def prereg():
+    email = None
+    if request.method = 'POST':
+        email = request.form['email']
+        reg = User(email)
+        db.session.add(reg)
+        db.session.commit()
+        return render_template('success.html')
+    return render_template('collect.html')
+
+@app.route('/return_emails', methods=['GET'])
+def return_emails():
+    all_emails = db.session.query(User.email).all()
+    return jsonify(all_emails)
+
 
 
 if __name__ == '__main__':
